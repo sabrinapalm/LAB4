44ef1658b87bd18bbc5c8752ada3c9193e3e9d33
Sabrina Palm <sabrina_palm@hotmail.com>
2017-12-11 00:28:59 +0100

    added more content and code

--- lab4.js
+++ lab4.js
@@ -1,1 +1,70 @@
-let search = document.getElementById("search");
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    
+    
+//request API key 
+    
+let apiBtn = document.getElementById("apibtn");
+    
+
+apiBtn.addEventListener('click', function(event){
+    
+    let apiKey = document.getElementById("api");
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = function(event) {
+    
+    if (req.readyState == 4) {
+        let ob = JSON.parse(req.responseText);
+        let key = ob.key;
+        api.innerHTML = `= ${key}`;
+    }        
+}
+req.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey');
+req.send();
+    
+})
+
+
+    
+//delete books
+const list = document.getElementsByTagName("ul")[0];
+
+list.addEventListener('click', function(event){
+    
+    if (event.target.className == 'delete') {
+        const li = event.target.parentElement;
+        list.removeChild(li);
+    }
+})
+
+
+
+
+//search for books
+
+const search = document.getElementById("search");
+
+search.addEventListener('keyup', function(event){
+    
+    const term = event.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        
+        if (title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
+}) 
+    
+    
+    
+    
+    
+});
+
