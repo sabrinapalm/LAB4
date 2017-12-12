7114cfbd9a3d64528238996f88a5eb3fcdee3798
Sabrina Palm <sabrina_palm@hotmail.com>
2017-12-12 02:55:07 +0100

    added getkey function and add function

--- lab4.js
+++ lab4.js
@@ -1,70 +1,100 @@
+//request API key  
+let key,
+    apibtn = document.getElementById("apibtn"),
+    apiKey = document.getElementById("api");
 
-document.addEventListener('DOMContentLoaded', function(){
-    
-    
-    
-//request API key 
-    
-let apiBtn = document.getElementById("apibtn");
-    
+//add books
+let addbtn = document.getElementById("addbtn"),
+    mylist = document.getElementById("mylist"),
+    title = document.getElementsByTagName("input")[0],
+    author = document.getElementsByTagName("input")[1],
+    result = document.getElementById("result"),
+    failed = 0;
 
-apiBtn.addEventListener('click', function(event){
+//change books
+let change = document.getElementById("change");
+
+
+
+
+//EVENT LISTENERES
+apibtn.addEventListener('click', function(event){
+    getKey() //calling getKey function
+});
+
+
+addbtn.addEventListener('click', function(event){
+    event.preventDefault();
+    addBook() //calling addBook function
+});
+
+
+change.addEventListener('click', function(event){  
+    changeBook(); //calling changeBook function
+});
+
+
+//FUNCTIONS 
+//get key function
+function getKey() {
     
-    let apiKey = document.getElementById("api");
     let req = new XMLHttpRequest();
+    
+    req.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey', true);
 
     req.onreadystatechange = function(event) {
     
-    if (req.readyState == 4) {
+    if (req.readyState == 4 && req.status == 200) {
         let ob = JSON.parse(req.responseText);
-        let key = ob.key;
-        api.innerHTML = `= ${key}`;
-    }        
+        key = ob.key;
+        api.innerHTML = `Recieved: ${key}`;
+    }  
 }
-req.open('GET', 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey');
-req.send();
+req.send();  
+}
+
+
+
+
+//add book function
+
+function addBook() {
     
-})
-
-
+    let addreq = new XMLHttpRequest(); 
     
-//delete books
-const list = document.getElementsByTagName("ul")[0];
-
-list.addEventListener('click', function(event){
+    addreq.open('GET', `https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=${key}&title=${title.value}&author=${author.value}`, true)
     
-    if (event.target.className == 'delete') {
-        const li = event.target.parentElement;
-        list.removeChild(li);
-    }
-})
-
-
-
-
-//search for books
-
-const search = document.getElementById("search");
-
-search.addEventListener('keyup', function(event){
-    
-    const term = event.target.value.toLowerCase();
-    const books = list.getElementsByTagName('li');
-    
-    Array.from(books).forEach(function(book){
-        const title = book.firstElementChild.textContent;
+    addreq.onload = function() {
         
-        if (title.toLowerCase().indexOf(term) != -1) {
-            book.style.display = 'block';
+    if (this.status == 200) {
+
+        let ob1 = JSON.parse(this.responseText)
+
+        if(ob1.status == "error") {
+
+            failed = failed + 1;
+            result.innerHTML = `Error message: ${ob1.message} <br />
+                                Failed: ${failed}`;
+
         } else {
-            book.style.display = 'none';
-        }
-    })
-}) 
+
+            let li = document.createElement("LI");
+            li.innerHTML = `Title: ${title.value} <br /> 
+                            Author: ${author.value} <br /> 
+                            ID: ${ob1.id}`;
+            
+            mylist.appendChild(li);
+            
+        }   
+    } 
+} 
+addreq.send();
+}
+
+
+//change book function
+function changeBook() {
     
-    
-    
-    
-    
-});
+}
+
 
